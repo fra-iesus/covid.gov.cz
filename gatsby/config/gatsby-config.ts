6147553139819246a9ede87aaf12f61c4a5e58e7
Karel Koudelka <k.koudelka01@gmail.com>
2020-11-02 14:46:14 +0000

    feat(FE): seo

--- gatsby/config/gatsby-config.ts
+++ gatsby/config/gatsby-config.ts
@@ -37,6 +37,31 @@ const config = {
       },
     },
     {
+      resolve: 'gatsby-plugin-seo',
+      options: {
+        siteName: 'Covid Portál',
+        defaultSiteImage: '/img/logo.png',
+        siteUrl: 'https://covidportal.cz',
+        // twitterCreator: '@twitterhandle',
+        // twitterSite: '@twitterhandle',
+        globalSchema: `{
+            "@type": "WebSite",
+            "@id": "https://example.com/#website",
+            "url": "https://example.com/",
+            "name": "Example Site Title",
+            "publisher": {
+              "@id": "https://example.com/about/#organization"
+            },
+            "image": {
+              "@type": "ImageObject",
+              "@id": "https://example.com/#logo",
+              "url": "https://example.com/img/logo.png",
+              "caption": "Example Company Logo"
+            }
+          }`,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-purgecss',
       options: {
         // develop: true,
